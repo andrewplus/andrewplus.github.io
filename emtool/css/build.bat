7d18b3f918606f4225a2f73f0a04c8c4ed8f0db7
Andrew <41998850+andrewplus@users.noreply.github.com>
2020-05-30 12:40:11 -0300

    Beta release of the new config editor

---
+++ emtool/css/build.bat
@@ -0,0 +1,2 @@
+del ..\desktop\emtool\app\assets\app.css
+copy simplebar.css + style.css /a ..\desktop\emtool\app\assets\app.css /b /y
---
+++ emtool/js/build.bat
@@ -0,0 +1,2 @@
+del ..\desktop\emtool\app\assets\app.js
+copy presets.js + polyfills.js + dictionary.js + simplebar.js + editor.js + mixin-add.js + preset-load.js + export.js /a ..\desktop\emtool\app\assets\app.js /b /y
