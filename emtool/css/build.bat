4e333e2bdfddbccb0408e3a226a7366e8553bf29
Andrew <41998850+andrewplus@users.noreply.github.com>
2020-09-10 22:38:06 -0300

    Moved emtool to docs.epicmickey.wiki

--- emtool/css/build.bat
+++
@@ -1,2 +0,0 @@
-del ..\desktop\emtool\app\assets\app.css
-copy simplebar.css + style.css /a ..\desktop\emtool\app\assets\app.css /b /y
--- emtool/js/build.bat
+++
@@ -1,2 +0,0 @@
-del ..\desktop\emtool\app\assets\app.js
-copy presets.js + polyfills.js + dictionary.js + simplebar.js + editor.js + mixin-add.js + preset-load.js + export.js /a ..\desktop\emtool\app\assets\app.js /b /y
